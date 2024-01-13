3c266d2cc3d68496f38224b912a3ed2ef860daed
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2024-01-13 11:35:42 -0300

    (fix) - dueDate

--- src/contracts/dto/contract.dto.ts
+++ src/contracts/dto/contract.dto.ts
@@ -2,10 +2,12 @@ import { PartialType } from '@nestjs/mapped-types';
 import { $Enums, Prisma } from '@prisma/client';
 
 export class CreateContractDto implements Prisma.ContractCreateInput {
+  Billing?: Prisma.BillingCreateNestedManyWithoutContractInput;
   value: number;
   status: $Enums.ContractStatus;
   startDate: string;
   endDate: string;
+  dueDate: string;
   user: Prisma.UserCreateNestedOneWithoutContractInput;
   property: Prisma.PropertyCreateNestedOneWithoutContractInput;
   renter: Prisma.RenterCreateNestedOneWithoutContractInput;
